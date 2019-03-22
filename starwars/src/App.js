116415e416ded59c7550f5595cbfad532fe71722
Oscar Pacheco <Oscarpa88@yahoo.com>
2019-03-22 13:30:41 -0500

    finished mvp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import StarWarsCharacters from './components/StarWarsCharacters';
+
 class App extends Component {
   constructor() {
     super();
@@ -30,9 +32,11 @@ class App extends Component {
   };
 
   render() {
+
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        < StarWarsCharacters  starwarsChars={this.state.starwarsChars}/>
       </div>
     );
   }
